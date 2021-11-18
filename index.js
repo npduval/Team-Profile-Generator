4f2d2cd564a938ec85cd901c31bcba66f5e0e4d9
npduval <nickman92186@yahoo.com>
2021-11-18 16:48:21 -0500

    updated each class and added inquirer questions

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 const inquirer = require('inquirer');
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'managerName',
+      message: 'What is your Manager\'s name?',
+    },
+    {
+      type: 'number',
+      message: 'What is your Manger\'s Employee ID?',
+      name: 'managerId',
+    },
+    {
+      type: 'input',
+      message: 'What is your Manger\'s Email Adress?',
+      name: 'managerEmail',
+    },
+    {
+    type: 'number',
+    message: 'What is your Manger\'s Office Number?',
+    name: 'managerOffice',
+    },
+    {
+        type: 'number',
+        message: 'What is your Manger\'s Office Number?',
+        name: 'managerOffice',
+    },
+    {
+        type: 'list',
+        message: 'Would you like to add an additional team member?',
+        name: 'addTeam',
+        choices: ['Intern', 'Engineer', "No, my team is complete"]
+    },
+]).then(awnsers => {
+    console.log(awnsers);
+
+})
