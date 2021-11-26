7bad5b932f6c13a1f5cf4eef0cbd2315832148e5
npduval <nickman92186@yahoo.com>
2021-11-26 13:41:12 -0500

    completed gerate html function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs'); 
+const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const { createInflate } = require('zlib');
 
 let colleages = [];
 
@@ -82,7 +84,8 @@ const CreateManager = () => {
     CreateColleague(addTeam);
   } else {
     console.log(colleages);
-    createFile(colleages);
+    createHTML(colleages);
+    
   }
 })
 
@@ -171,32 +174,109 @@ const CreateColleague = (role) => {
       const {name , id, email, gitHub, school, addTeam} = empData;
         
       if (role === 'Engineer') {
-          let employee = new Engineer(name, id, email, gitHub);
-          colleages.push(employee);
+          let engineer = new Engineer(name, id, email, gitHub);
+          colleages.push(engineer);
       } else if (role === "Intern") {
-          let employee = new Intern(name, id, email, school); 
-          colleages.push(employee);     
+          let intern = new Intern(name, id, email, school); 
+          colleages.push(intern);     
       } 
        
       if (addTeam === "Engineer" || addTeam ==="Intern") {
         CreateColleague(addTeam);
       } else {
         console.log(colleages);
-        createFile(colleages);
+        createHTML(colleages);
       }
     })
 
     
   };
 
-    // const createHTML = () => {
+
+    const createHTML = (team) => {
+
+      let file = [`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+          <link href="../src/style.css" rel="stylesheet">
+          <title>Template</title>
+      </head>
+      <body>
+          <nav class="navbar navbar-expand-xlg navbar-light bg-info">
+              <div class="container-md">
+                <span class="navbar-brand" id="navbar">Team Profile</span>
+              </div>
+          </nav>
+          <main>
+              <div class="container-fluid">
+                  <div class="row justify-content-center" id="cards">`];
+
+      for ( i = 0; i < team.length; i++) {
+      //DEVNOTE - string litterals all undefined
+        if (team[i] instanceof Manager) {
+         
+          const data = `<div class="col-4 mt-5">
+          <div class="card  h-100" style="width: 18rem;">
+              <div class="card-header text-dark bg-info mb-3">
+              Manager
+              </div>
+              <ul class="list-group list-group-flush">
+              <li class="list-group-item">Name: ${team[i].name}</li>
+              <li class="list-group-item">ID: ${team[i].id}</li>
+              <li class="list-group-item">Email: ${team[i].email}</li>
+              <li class="list-group-item">Office: ${team[i].officeNumber}</li>
+              </ul>
+          </div>
+      </div>`;
+          file.push(data);
+        } else if (team[i] instanceof Engineer) {
+          
+          const data = `<div class="col-4 mt-5">
+          <div class="card  h-100" style="width: 18rem;">
+              <div class="card-header text-dark bg-info mb-3">
+              Engineer
+              </div>
+              <ul class="list-group list-group-flush">
+              <li class="list-group-item">Name: ${team[i].name}</li>
+              <li class="list-group-item">ID: ${team[i].id}</li>
+              <li class="list-group-item">Email: ${team[i].email}</li>
+              <li class="list-group-item">GitHub: ${team[i].gitHub}</li>
+              </ul>
+          </div>
+      </div>`;
+          file.push(data);
+        } else if (team[i] instanceof Intern) {
+         
+         const data = `<div class="col-4 mt-5">
+          <div class="card  h-100" style="width: 18rem;">
+              <div class="card-header text-dark bg-info mb-3">
+              Intern
+              </div>
+              <ul class="list-group list-group-flush">
+              <li class="list-group-item">Name: ${team[i].name}</li>
+              <li class="list-group-item">ID: ${team[i].id}</li>
+              <li class="list-group-item">Email: ${team[i].email}</li>
+              <li class="list-group-item">School: ${team[i].school}</li>
+              </ul>
+          </div>
+      </div>`;
+        file.push(data);
+        }
+
+      }
+
+      let footer = `  </div></div></main></body></html>`
+      file.push(footer);
+      let full = file.join();
+
+      createFile(full);
 
 
-    // }
+    }
 
-  const createFile = (team) => {
-  const data = JSON.stringify(team);
-  fs.writeFile('./dist/index.html', data, (err) =>
+  const createFile = (html) => {
+  
+  fs.writeFile('./dist/index.html', html, (err) =>
   err ? console.error(err) : console.log('Successfully created team profile')
   ) 
 };
